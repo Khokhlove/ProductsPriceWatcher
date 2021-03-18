5f406a5437b72dda2a4c1d4b6e7ae5f53c1d408a
Алексей Хохлов <Akhokhlove@gmail.com>
2021-03-18 15:17:30 +0700

    Recode

--- HtmlParser/ProductEditor.cs
+++ HtmlParser/ProductEditor.cs
@@ -32,7 +32,7 @@ namespace HtmlParser
 
         private void ButtonAdd_Click(object sender, EventArgs e)
         {
-
+            ConnectionDateBase(textBox1.Text, textBox2.Text, "10");
         }
 
         private void AddDataBase(string nameObject, string url)
@@ -40,14 +40,28 @@ namespace HtmlParser
             
         }
 
-        private void ConnectionDateBase()
+        private void ConnectionDateBase(string o, string u, string i)
         {
-            SqlConnection db = new SqlConnection(@"C:\Users\alexe\source\repos\HtmlParser\HtmlParser\Database1.mdf";
+            string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\RepositoryVisualStudio\ProductsPriceWatcher\HtmlParser\Database1.mdf;Integrated Security=True";
+            string sqlExpression = "";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand(sqlExpression, connection);
+
+            }
+            string objectName = o;
+            string urlSite = u;
+            string siteId = i;
+            SqlConnection db = new SqlConnection(@"C:\Users\alexe\source\repos\HtmlParser\HtmlParser\Database1.mdf");
             db.Open();
 
-            SqlCommand command = new SqlCommand("INSERT * INTO Object (Object, UrlSite, SiteId) VALUES(@)");
-        
+            // command = new SqlCommand("INSERT * INTO Object (Object, UrlSite, SiteId) VALUES(@objectName, @urlSite, @siteId)");
 
-
+            int age = 23;
+            string name = "T',10);INSERT INTO Users (Name, Age) VALUES('H";
+            string sqlExpression = "INSERT INTO Users (Name, Age) VALUES (@name, @age)";
+           
     }
 }
