76d42dec8fe9ddc97d870bb0c997d330f0a8e98c
ablaev.dmitry <dablaev@yandex.ru>
2021-03-18 14:57:11 +0700

    Добавлены классы-контейнеры для товаров и магазинов

---
+++ HtmlParser/src/CitilinkShop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CsQuery;
+
+namespace HtmlParser
+{
+    public class CitilinkShop : Shop
+    {
+        public CitilinkShop(string link) : base("Citilink", link) { }
+
+        public override int GetPrice()
+        {
+            CQ citilinkDom = CQ.CreateFromUrl(this.link);
+            CQ citilinPriceNode = citilinkDom[".ProductHeader__price-default_current-price"];
+            if (citilinPriceNode.Length > 0)
+            {
+                return Convert.ToInt32(citilinPriceNode.Text().Trim().Replace(" ", ""));
+            }
+
+            return -1;
+        }
+    }
+}
